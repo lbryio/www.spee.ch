503cad75bc87b374a50ef716cce2b9d05d8ce22e
bill bittner <bittner.w@gmail.com>
2018-03-09 09:43:10 -0800

    split up mysql and site configs

---
+++ config/index.js
@@ -0,0 +1,7 @@
+const mysqlConfig = require('./mysqlConfig');
+const siteConfig = require('./siteConfig');
+
+module.exports = {
+    mysqlConfig,
+    siteConfig,
+}
---
+++ config/siteConfig.js
@@ -0,0 +1,26 @@
+module.exports = {
+    analytics: {
+        googleId: 'UA-60403362-6',
+    },
+    publishing: {
+        primaryClaimAddress     : 'bDZ2wPwtULUGxT7GXuNLpQhXmdPRUTUkcL',
+        additionalClaimAddresses: ['banpwixPosfVDWnGvXqU2af36Qpsd7buGd'],
+        thumbnailChannel        : '@dev1thumbs',
+        thumbnailChannelId      : 'aeb625ff6f66c3eeeb42885070f4e53876033626',
+        uploadDirectory: '/home/lbry/Uploads',
+    },
+    details: {
+        title      : 'Spee.ch',
+        name       : 'dev1.Spee.ch',
+        host       : 'https://dev1.spee.ch',
+        description: 'Open-source, decentralized image and video sharing.',
+    },
+    openGraph: {
+        defaultClaimTitle      : 'dev1 Spee.ch',
+        defaultClaimThumbnail  : 'https://spee.ch/assets/img/video_thumb_default.png',
+        defaultClaimDescription: 'Open-source, decentralized image and video sharing.',
+    },
+    session: {
+        sessionKey: 'nans$#kfjanwe234rydns',
+    },
+};
