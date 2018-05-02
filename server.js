d4d93ccbba1e194630443e46528a0dd977b192d4
bill bittner <bittner.w@gmail.com>
2018-05-02 10:50:59 -0700

    added views config file and updated others to read from json

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
+const fs = require('fs');
+
 // require the spee.ch package
 const Server = require('spee.ch');
+
 // get the local configs
-const loggerConfig = require('./config/loggerConfig.js'); // make these direct
-const mysqlConfig = require('./config/mysqlConfig.js');
-const siteConfig = require('./config/siteConfig.js');
-const slackConfig = require('./config/slackConfig.js');
+loggerConfigJSON = fs.readFileSync('./cli/output/loggerConfig.json');
+const loggerConfig = JSON.parse(loggerConfigJSON);
+
+mysqlConfigJSON = fs.readFileSync('./cli/output/mysqlConfig.json');
+const mysqlConfig = JSON.parse(mysqlConfigJSON);
+
+siteConfigJSON = fs.readFileSync('./cli/output/siteConfig.json');
+const siteConfig = JSON.parse(siteConfigJSON);
+
+slackConfigJSON = fs.readFileSync('./cli/output/slackConfig.json');
+const slackConfig = JSON.parse(slackConfigJSON);
+
+// get custom pages
+const viewsConfig = require('./custom/views');
 
 try {
     // create a new spee.ch server
@@ -14,8 +27,7 @@ try {
     server.configureMysql(mysqlConfig);
     server.configureSite(siteConfig);
     server.configureSlack(slackConfig);
-    server.configureModels();
-    server.configureRoutes();
+    server.configureViews(viewsConfig);
     // create the express app and configure it
     server.initialize();
     // sync the db and start express app listening on the configured port
