769ac330a6d54f2e22239fe5f071a600a275e695
bill bittner <bittner.w@gmail.com>
2018-05-06 10:41:49 -0700

    added missing index files to git

---
+++ custom/views/index.js
@@ -0,0 +1,9 @@
+const components = require('./components');
+const containers = require('./containers');
+const pages = require('./pages');
+
+module.exports = {
+    components,
+    containers,
+    pages,
+}
---
+++ custom/views/pages/index.js
@@ -0,0 +1,5 @@
+import AboutPage from './AboutPage';
+
+module.exports = {
+    AboutPage,
+};
