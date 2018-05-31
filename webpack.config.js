e7ae70d7485ba5ffa9c0e152abc1fc2a5b934e74
bill bittner <bittner.w@gmail.com>
2018-05-30 18:06:47 -0700

    fixed public path in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const Path = require('path');
 const fs = require('fs');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const createModuleAliases = require('./utils/createModuleAliases.js');
 const SCSS_ROOT = Path.join(__dirname, 'node_modules/spee.ch/client/scss/');
 
@@ -9,23 +10,27 @@ module.exports = {
   target: 'web',
   entry : ['babel-polyfill', 'whatwg-fetch', './build/client.js'],
   output: {
-    path      : Path.join(__dirname, 'public/bundle/'),
-    publicPath: 'public/bundle/',
+    path      : Path.join(__dirname, 'public/bundle'),
+    publicPath: '/bundle/',
     filename  : 'bundle.js',
   },
   module: {
     rules:[
       {
         test: /\.scss$/,
-        use: ['style-loader','css-loader', 'sass-loader'],
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
       },
       {
-        test: /\.(png|jpg|gif|ttf|svg)$/,
+        test: /\.(png|jpg|gif|otf|ttf|svg)$/,
         use: [
           {
             loader: 'url-loader',
             options: {
-              limit: 8192
+              limit: 8192,
+              name: '[name]-[hash].[ext]',
             }
           }
         ]
@@ -41,4 +46,7 @@ module.exports = {
     alias: customAliases,
     extensions: ['.js', '.jsx', '.scss'],
   },
+  plugins: [
+    new ExtractTextPlugin('style.css'),
+  ]
 };
